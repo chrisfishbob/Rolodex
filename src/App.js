0ec10afd64c44ace0b0a0e9e9fe99b2c1d2801c3
Chris <chrisfishbob@gmail.com>
2022-07-06 16:40:08 -0700

    Search Bar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { waitFor } from "@testing-library/react";
 // Component is a self contained piece of code that returns some
 // visual UI representation of HTML
 class App extends Component {
+  // Constructor runs first
   constructor() {
     super();
 
@@ -16,22 +17,37 @@ class App extends Component {
 
   // Mounting is the first time a component gets placed on the DOM
   // Remounted can only happen if remounted
+  // This runs third
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((Response) => Response.json())
       .then((users) =>
         this.setState(() => {
-          return { monsters: users};
-        },
-        () => {
-          console.log(this.state)
+          return { monsters: users };
         })
       );
   }
 
+  // This runs second
   render() {
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search monsters"
+          onChange={(event) => {
+            // "aAaA" => "aaaa"
+            const searchString = event.target.value.toLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return {monsters: filteredMonsters};
+            })
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
